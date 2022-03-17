c8d3ea5e9c300b6620694f21e31b9288d5954849
Vitor Costa <vitor.costa@exaud.com>
2022-03-17 10:52:46 +0000

    dinamyc filling overview template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -33,21 +33,63 @@ const url = require(`url`);
 // console.log(`Will read the file!`); // 1st presented
 //////////////////////////////////////////////////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 // executed once so it may be done synchronously
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  `utf-8`
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  `utf-8`
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  `utf-8`
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
-const dataObj = JSON.parse(data);
+const dataObj = JSON.parse(data); // the data become an array of obj
 
 // server creation
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
   // routing in the server
+  // Overview page
   if (pathName === `/` || pathName === `/overview`) {
-    res.end(`This is the OVERVIEW`);
+    res.writeHead(200, { "Content-type": `text/html` });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    // console.log(cardsHtml);
+    res.end(output);
+
+    // Product page
   } else if (pathName === `/product`) {
     res.end(`This is the PRODUCT`);
+
+    // API
   } else if (pathName === `/api`) {
     res.writeHead(200, { "Content-type": `application/json` });
     res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": `text/html`,
@@ -56,6 +98,7 @@ const server = http.createServer((req, res) => {
     res.end(`<h1>Page not Found!</h1>`);
   }
 });
+
 // server listening to requests
 server.listen(8000, `127.0.0.1`, () => {
   console.log(`Listening to requests on port 8000`);
