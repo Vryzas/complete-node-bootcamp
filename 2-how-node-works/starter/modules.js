caebf1723c1bd95a62f867db3a7c485e49b554cd
Vitor Costa <vitor.costa@exaud.com>
2022-03-18 17:23:29 +0000

    modules-2

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,6 +1,12 @@
 // module.exports
 const C = require("./test-module-1");
 const calc = new C();
-console.log(calc.add(3, 2));
-console.log(calc.multiply(3, 2));
-console.log(calc.divide(10, 2));
+console.log(calc.add(5, 5));
+console.log(calc.multiply(5, 2));
+console.log(calc.divide(10, 1));
+
+// exports
+const calc2 = require("./test-module-2");
+console.log(calc2.add(1, 1));
+console.log(calc2.multiply(1, 2));
+console.log(calc2.divide(10, 5));
