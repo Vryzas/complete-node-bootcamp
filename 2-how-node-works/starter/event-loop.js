4514936d9e7728d85590c64b3adfee3bd1c7e36c
Vitor Costa <vitor.costa@exaud.com>
2022-03-18 11:11:10 +0000

    threadpool manipulation

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,4 +1,8 @@
 const fs = require(`fs`);
+const crypto = require(`crypto`);
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4; // setting the number of threads
 
 setTimeout(() => console.log(`Timer 1 finished`), 0); // expires after 0 secs
 setImmediate(() => console.log(`Immediate 1 finished`)); // expires immediately
@@ -10,5 +14,14 @@ fs.readFile("test-file.txt", () => {
   setImmediate(() => console.log(`Immediate 2 finished`)); // setImmediate callback
 
   process.nextTick(() => console.log(`Process.nextTick`));
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password 1 encrypted");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password 2 encrypted");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password 3 encrypted");
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "password 4 encrypted");
 }); // callback fn
 console.log(`Hello from the top level code!`); // top level code
