ebc5e7b8fd96580ba6b196f83ad2dcd45e914e4f
Vitor Costa <vitor.costa@exaud.com>
2022-03-18 10:46:06 +0000

    event-loop practice

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,1 +1,14 @@
+const fs = require(`fs`);
 
+setTimeout(() => console.log(`Timer 1 finished`), 0); // expires after 0 secs
+setImmediate(() => console.log(`Immediate 1 finished`)); // expires immediately
+fs.readFile("test-file.txt", () => {
+  console.log(`I/O finished`); // I/O callback
+  console.log(`---------------------------------------------`);
+  setTimeout(() => console.log(`Timer 2 finished`), 0); // expired timer callback
+  setTimeout(() => console.log(`Timer 3 finished`), 3000); // 3 sec timer
+  setImmediate(() => console.log(`Immediate 2 finished`)); // setImmediate callback
+
+  process.nextTick(() => console.log(`Process.nextTick`));
+}); // callback fn
+console.log(`Hello from the top level code!`); // top level code
