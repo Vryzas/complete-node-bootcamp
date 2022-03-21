a05d8b2d2d13b25b129ef438a50a3bf0fa325f98
Vitor Costa <vitor.costa@exaud.com>
2022-03-21 10:34:43 +0000

    corrections-1

--- 1-node-farm/starter/modules/replaceTemplate.js
+++ 1-node-farm/starter/modules/replaceTemplate.js
@@ -8,7 +8,8 @@ module.exports = (temp, product) => {
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic)
+  if (!product.organic) {
     output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
   return output;
 };
