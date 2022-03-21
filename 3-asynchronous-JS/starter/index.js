f3c7c3c6bba3b31312b7f8022ce2f0072ac1d568
Vitor Costa <vitor.costa@exaud.com>
2022-03-21 13:16:04 +0000

    chaining_promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -12,23 +12,29 @@ const readFilePro = (file) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`).then((data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      // calback to promise transformation
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) {
-          return console.log(err.message);
-        }
-        console.log(`Random dog img saved to file!`);
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) {
+        reject('Could not write the file');
+      }
+      resolve('Sucessfully wrote the file');
     });
-});
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`) // promise
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`); // returns another promise
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message); // returns another promise
+  })
+  .then(() => {
+    console.log(`Random dog image saved to file!`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
