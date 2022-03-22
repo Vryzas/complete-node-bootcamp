cd6c35adb00e546ca9558117d2edde119743e94a
Vitor Costa <vitor.costa@exaud.com>
2022-03-22 16:58:45 +0000

    Added user routing methods

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -93,6 +93,37 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
 app.route('/api/v1/tours').get(getAllTours).post(postTour);
 
 // accessing one particular tour with url params
