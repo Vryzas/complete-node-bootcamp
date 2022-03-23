384cda159fd1e0455c1244bd55dac1b4abe3629c
Vitor Costa <vitor.costa@exaud.com>
2022-03-23 14:41:51 +0000

    ID check simplified into param middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -4,6 +4,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -17,12 +27,6 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: 'success',
@@ -54,12 +58,6 @@ exports.postTour = (req, res) => {
 };
 
 exports.patchTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(200).json({
     status: 'success',
     tour: '<Updated tour would go here...>',
@@ -67,12 +65,6 @@ exports.patchTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null,
--- 4-natours/starter/routes/tourRouter.js
+++ 4-natours/starter/routes/tourRouter.js
@@ -4,10 +4,7 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 router.route('/').get(tourController.getAllTours).post(tourController.postTour);
 
-router.param('id', (req, res, next, val) => {
-  console.log(`tour id is ${val}`);
-  next();
-});
+router.param('id', tourController.checkID);
 
 router
   .route('/:id')
