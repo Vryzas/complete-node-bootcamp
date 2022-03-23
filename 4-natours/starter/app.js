91ae2ca3e64dfda36ead69c6bb6b91133b998b52
Vitor Costa <vitor.costa@exaud.com>
2022-03-23 15:27:43 +0000

    Added static file middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 });
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
