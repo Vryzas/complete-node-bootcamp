72ed6a20e8dc951f23590985ccf45b22a991f746
Vitor Costa <vitor.costa@exaud.com>
2022-03-23 13:03:49 +0000

    Placing server code into separate file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -16,7 +16,4 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App listening on port ${port}...`);
-});
+module.exports = app;
