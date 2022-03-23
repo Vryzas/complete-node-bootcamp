5c7f5f87a3c80131b3a8036d02d5c1ab41ab6704
Vitor Costa <vitor.costa@exaud.com>
2022-03-23 14:27:09 +0000

    Added param middleware to tourRouter

--- 4-natours/starter/routes/tourRouter.js
+++ 4-natours/starter/routes/tourRouter.js
@@ -4,6 +4,11 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 router.route('/').get(tourController.getAllTours).post(tourController.postTour);
 
+router.param('id', (req, res, next, val) => {
+  console.log(`tour id is ${val}`);
+  next();
+});
+
 router
   .route('/:id')
   .get(tourController.getTour)
