00cdeacfb374a292c3b313b1a10a643e6f1b7a2c
Vitor Costa <vitor.costa@exaud.com>
2022-03-24 15:25:04 +0000

    Setting a tour schema/model

--- 4-natours/after-section-06/server.js
+++ 4-natours/after-section-06/server.js
@@ -19,6 +19,24 @@ mongoose
     console.log(`DB connection successfully established`);
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour musta have a name!'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour musta have a price!']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
