6a8c833319ce68e0b314f8461b49d7b415f4f1de
Vryzas <vryzas27@gmail.com>
2022-03-24 22:27:10 +0000

    Query results filtering

--- 4-natours/after-section-06/controllers/tourController.js
+++ 4-natours/after-section-06/controllers/tourController.js
@@ -2,7 +2,11 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(queryObj);
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
