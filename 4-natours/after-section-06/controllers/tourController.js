ef67b4b35020d3f7cf6858be3ccd1ba514628b88
Vryzas <vryzas27@gmail.com>
2022-03-24 23:40:20 +0000

    Comments added for better method readability

--- 4-natours/after-section-06/controllers/tourController.js
+++ 4-natours/after-section-06/controllers/tourController.js
@@ -1,20 +1,23 @@
 const Tour = require('./../models/tourModel');
 
+// query multiple results (all as default)
 exports.getAllTours = async (req, res) => {
   try {
+    // FILTERING THE QUERY
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach(el => delete queryObj[el]);
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
     let query = Tour.find(JSON.parse(queryStr));
-    // await console.log(query);
+    // SORTING
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
     }
+    // FIELDS SELECTION
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
@@ -22,6 +25,7 @@ exports.getAllTours = async (req, res) => {
       // removes fields not to be shown in client (ie: passwords, pins...)
       query = query.select('-__v');
     }
+    // PAGINATION
     const page = req.query.page * 1 || 1;
     const limit = req.query.limit * 1 || 100;
     const skip = (page - 1) * limit;
@@ -32,7 +36,9 @@ exports.getAllTours = async (req, res) => {
         throw new Error('This page does not exist!');
       }
     }
+    // EXECUTE THE QUERY
     const tours = await query;
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
