a1b68f659fbd41d9cd8e1b0ebb7d4225f70c0041
Vitor Costa <vitor.costa@exaud.com>
2022-03-25 13:06:06 +0000

    Added a document middleware example

--- 4-natours/after-section-06/models/tourModel.js
+++ 4-natours/after-section-06/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -63,6 +65,12 @@ const tourSchema = new mongoose.Schema(
 // virtual properties are not added to the database and not usable in querys
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+// doc middleware, work for .save() or .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
