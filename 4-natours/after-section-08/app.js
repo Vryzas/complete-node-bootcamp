d0fa3e899791cd63499f024f272e4f4e6167e51a
Vitor Costa <vitor.costa@exaud.com>
2022-03-28 10:54:24 +0100

    Added middleware to handle unknown URL's

--- 4-natours/after-section-08/app.js
+++ 4-natours/after-section-08/app.js
@@ -23,4 +23,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// if the route isn't found, run this middleware
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
+});
+
 module.exports = app;
