06d43d5ee7879fe4694eac05b9b74f4b0097e033
Vryzas <vryzas27@gmail.com>
2022-03-30 23:30:32 +0100

    Added user update method

--- 4-natours/after-section-09/controllers/userController.js
+++ 4-natours/after-section-09/controllers/userController.js
@@ -22,6 +22,29 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     }
   });
 });
+
+exports.updateMe = async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+};
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
