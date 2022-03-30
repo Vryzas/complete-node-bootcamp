e6d26a91b60b12806c91851047565958e58609a9
Vryzas <vryzas27@gmail.com>
2022-03-30 23:29:56 +0100

    Added fiedl filtering for user update

--- 4-natours/after-section-09/controllers/userController.js
+++ 4-natours/after-section-09/controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
