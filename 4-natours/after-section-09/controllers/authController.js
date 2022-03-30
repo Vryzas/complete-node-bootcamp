6af52b83604220724fc6f5e19227114e57244708
Vitor Costa <vitor.costa@exaud.com>
2022-03-30 14:21:12 +0100

    Added user role to restrict deleteTour

--- 4-natours/after-section-09/controllers/authController.js
+++ 4-natours/after-section-09/controllers/authController.js
@@ -81,5 +81,18 @@ exports.protect = catchAsync(async (req, res, next) => {
     );
   }
   // grant's acces to protected route
+  req.user = freshUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // role comes from the last middleware
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action!', 403)
+      );
+    }
+    next();
+  };
+};
