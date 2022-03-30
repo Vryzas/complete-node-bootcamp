ed535f1659dcc272f361af264455c716390fd32c
Vryzas <vryzas27@gmail.com>
2022-03-30 22:54:13 +0100

    Added password update method

--- 4-natours/after-section-09/controllers/authController.js
+++ 4-natours/after-section-09/controllers/authController.js
@@ -169,3 +169,22 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1 get user from collections
+  const user = await User.findById(req.user.id).select('+password');
+  // 2 current password check
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong!', 401));
+  }
+  // 3 if ok, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // 4 log in user with new JWT
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'sucess',
+    token
+  });
+});
