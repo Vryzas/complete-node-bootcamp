76d0bd46784339372c6ede4380b17c1bac38db62
Vitor Costa <vitor.costa@exaud.com>
2022-03-31 12:42:22 +0100

    Added separators

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,9 +9,11 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
