4482a15a97eb7422a798d133338b8fb2c6f43ebb
Vitor Costa <vitor.costa@exaud.com>
2022-03-31 13:04:03 +0100

    Title case Corrected in tourController

--- 4-natours/after-section-06/controllers/tourController.js
+++ 4-natours/after-section-06/controllers/tourController.js
@@ -20,13 +20,13 @@ exports.getAllTours = async (req, res) => {
     const tours = await features.query;
     // SEND RESPONSE
     res.status(200).json({
-      status: 'success',
+      status: 'Success',
       results: tours.length,
       data: { tours }
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
@@ -36,12 +36,12 @@ exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
     res.status(200).json({
-      status: 'success',
+      status: 'Success',
       data: { tour }
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
@@ -51,14 +51,14 @@ exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
     res.status(201).json({
-      status: 'success',
+      status: 'Success',
       data: {
         tour: newTour
       }
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
@@ -71,14 +71,14 @@ exports.updateTour = async (req, res) => {
       runValidators: true
     });
     res.status(200).json({
-      status: 'success',
+      status: 'Success',
       data: {
         tour: tour
       }
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
@@ -88,12 +88,12 @@ exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
-      status: 'success',
+      status: 'Success',
       data: null
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
@@ -121,14 +121,14 @@ exports.getTourStats = async (req, res) => {
       }
     ]);
     res.status(200).json({
-      status: 'success',
+      status: 'Success',
       data: {
         stats
       }
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
@@ -172,14 +172,14 @@ exports.getMonthlyPlan = async (req, res) => {
       }
     ]);
     res.status(200).json({
-      status: 'success',
+      status: 'Success',
       data: {
         plan
       }
     });
   } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: 'Fail',
       message: err
     });
   }
