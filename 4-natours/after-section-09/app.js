a2e02dc96ad34f26e5e835c298e0879fef1ade5f
Vitor Costa <vitor.costa@exaud.com>
2022-03-31 11:20:09 +0100

    Added no parameter pollution package/feature

--- 4-natours/after-section-09/app.js
+++ 4-natours/after-section-09/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +34,19 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
+// prevents parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      ' ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 // serves static files
 app.use(express.static(`${__dirname}/public`));
 
