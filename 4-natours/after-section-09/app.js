b9f9d4568aa765b047f679a52d30b9285d774129
Vitor Costa <vitor.costa@exaud.com>
2022-03-31 11:09:55 +0100

    Added data sanitizations features

--- 4-natours/after-section-09/app.js
+++ 4-natours/after-section-09/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,8 +27,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
-
+// Body parser to read the request body data
 app.use(express.json({ limit: '10kb' }));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// serves static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
